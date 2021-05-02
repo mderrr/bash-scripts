4d34ada1b80598c83f43b416cea3c45263a098a3
shernandezz <santihernandezzapata@gmail.com>
2021-05-02 00:12:38 -0500

    Updated gnome-profile

--- gnome-profile.sh
+++ gnome-profile.sh
@@ -1,50 +1,79 @@
 #!/bin/bash
 
-SCRIPT_VERSION="1.3"
+SCRIPT_VERSION="1.4"
 SCRIPT_NAME="Gnome Profile"
-HELP_MESSAGE="\n$SCRIPT_NAME $SCRIPT_VERSION, an Archlinux Terminal Profile Utility\nUsage: gnome-profile [Options]... [Custom Path]\n\nOptions:\n -V, --version\t\tDisplay script version.\n -h, --help\t\tShow this help message.\n -I, --import\t\tImport profile from file.\n -E, --export\t\tExport profile to file. (Default)\n"
-VERSION_MESSAGE="$SCRIPT_NAME version $SCRIPT_VERSION"
+
+HELP_MESSAGE="\n%s %s, an Archlinux Terminal Profile Utility\nUsage: gnome-profile [Options]... [Custom Path]\n\nOptions:\n -V, --version\t\tDisplay script version.\n -h, --help\t\tShow this help message.\n -I, --import\t\tImport profile from file.\n -E, --export\t\tExport profile to file. (Default)\n\n"
+VERSION_MESSAGE="%s version %s\n"
+OPTION_NOT_RECOGNIZED_MESSAGE="Option %s not recognized\n"
+
+PROFILE_EXPORTED_SUCCESSFULLY_MESSAGE="Profile exported successfully.\n"
+PROFILE_IMPORTED_SUCCESSFULLY_MESSAGE="Profile imported successfully.\n"
+CUSTOM_FOLDER_NOT_FOUND_MESSAGE="Folder '%s' not found, aborting profile %s.\n"
+
+IMPORT="import"
+EXPORT="export"
+
+PROFILE_FILE_NAME="gnome-terminal-profiles.dconf"
 
 function getFilePath() {
-    profile_file="gnome-terminal-profiles.dconf"
+    local custom_path=${1:-"."}
+    local profile_file_name=$PROFILE_FILE_NAME
+    local profile_file_path="$custom_path/$profile_file_name"
 
-    if [ ! -z "$1" ]; then
-        profile_file="$1/$profile_file"
+    if [[ ! -d "$custom_path" ]]; then
+        profile_file_path=""
     fi
 
-    echo "$profile_file"
+    echo "$profile_file_path"
 }
 
 function exportProfile() {
-    profile_file="$(getFilePath $1)"
-    dconf dump /org/gnome/terminal/legacy/profiles:/ > "$profile_file"
+    local profile_file_path=$1
 
-    echo "Profile exported successfully."
+    dconf dump /org/gnome/terminal/legacy/profiles:/ > "$profile_file_path"
+    printf "$PROFILE_EXPORTED_SUCCESSFULLY_MESSAGE"
 }
 
 function importProfile() {
-    profile_file="$(getFilePath $1)"
-    dconf load /org/gnome/terminal/legacy/profiles:/ < "$profile_file"
+    local profile_file_path=$1
 
-    echo "Profile imported successfully."
+    dconf load /org/gnome/terminal/legacy/profiles:/ < "$profile_file_path"
+    printf "$PROFILE_IMPORTED_SUCCESSFULLY_MESSAGE"
+}
+
+function setupProfile() {
+    local action_name=$1
+    local custom_path=$2
+    local profile_file_path="$(getFilePath $custom_path)"
+
+    if [[ -z $profile_file_path ]]; then
+        printf "$CUSTOM_FOLDER_NOT_FOUND_MESSAGE" "$custom_path" "$action_name" & exit
+    fi
+
+    if [[ $action_name == $EXPORT ]]; then
+        exportProfile "$profile_file_path"
+    elif [[ $action_name == $IMPORT ]]; then
+        importProfile "$profile_file_path"
+    fi
 }
 
 while [[ "$1" =~ ^- ]]; do
 	case "$1" in
 
-        -h | --help) echo -e $HELP_MESSAGE & exit ;;
+        -h | --help) printf "$HELP_MESSAGE" "$SCRIPT_NAME" "$SCRIPT_VERSION" & exit ;;
         
-        -V | --version) echo -e $VERSION_MESSAGE & exit ;;
+        -V | --version) printf "$VERSION_MESSAGE" "$SCRIPT_NAME" "$SCRIPT_VERSION" & exit ;;
 
-        -I | --import) importProfile $2 && exit ;;
+        -I | --import) setupProfile "$IMPORT" $2 && exit ;;
 
-        -E | --export) exportProfile $2 && exit ;;
+        -E | --export) setupProfile "$EXPORT" $2 && exit ;;
 
-        -*) echo "Option $1 not recognized" & exit ;;
+        -*) printf "$OPTION_NOT_RECOGNIZED_MESSAGE" "$1" & exit ;;
 
 	esac
 
 	shift
 done
 
-exportProfile $1
+setupProfile "$EXPORT" "$1"
