050d83b19d257302ab75bb8227f3afac0b4a5648
shernandezz <santihernandezzapata@gmail.com>
2021-05-11 18:36:00 +0000

    Update

--- pacman-updates.sh
+++ pacman-updates.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export HISTIGNORE='*sudo -S*'
+
+passwd=$(/home/$USER/.scripts/pass.sh) &> /dev/null
+
+echo "$passwd" | sudo -S -k pacman -Sy &> /dev/null
 
 number_of_updates=$(pacman -Qu | wc -l)
 echo $number_of_updates
